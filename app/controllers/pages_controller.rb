0a348f6505994bd6a7aedd2fe941501b8f84c2c5
pedorgrande <pete@thecoderfactory.com>
2015-10-01 16:44:09 +1000

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-	skip_before_action :authenticate_user!, only: [:home, :contact, :thanks]
+	skip_before_action :authenticate_user!, only: [:home, :contact, :thanks, :about]
 	layout 'home', only: :home
   def home
   end
