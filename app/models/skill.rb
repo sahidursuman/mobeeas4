cf8afc655ca7e55fd81451fe94a2d7bc9fba8710
pedorgrande <pete@thecoderfactory.com>
2015-10-11 12:19:49 +1100

    upa

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,7 @@ class Skill < ActiveRecord::Base
   belongs_to :skill_category
   has_many :opportunity_skills
   has_many :opportunities, through: :opportunity_skills
-  has_many :candidate_skills
+  has_many :candidate_skills, dependent: :destroy
   has_many :users, through: :candidate_skills
 
   def self.alphabetical
