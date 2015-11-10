1d8dda86f6c419f717815122dccc54e3970cc68e
ksilitonga@hotmail.com <ksilitonga@hotmail.com>
2015-11-11 00:06:11 +1100

    fixed adding and deleting a host (not admin) to an organisation.

--- app/models/org_user.rb
+++ app/models/org_user.rb
@@ -1,4 +1,8 @@
 class OrgUser < ActiveRecord::Base
   belongs_to :organisation
   belongs_to :user
+
+  scope :admin_user,->{where(admin_status: true, verified_status: true)}
+  scope :host_user,->{where(admin_status: false, verified_status: true)}
+
 end
