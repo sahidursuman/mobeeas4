46ddf7adf33b2f5302dfa1f1b5deb254722dc4ef
ksilitonga@hotmail.com <ksilitonga@hotmail.com>
2016-03-09 13:34:56 +1100

    modified the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,69 @@
+role1 = Role.create!(name: 'admin')
+role2 = Role.create!(name: 'candidate')
+role3 = Role.create!(name: 'host')
+role4 = Role.create!(name: 'sponsor')
+
+
 user = User.create!(email: 'admin@mobeeas.com', password: 'KFmiwork$16')
 user.add_role :admin
+admin = Admin.create!(user_id: user.id, first_name: 'Adriana', last_name: 'Aster', phone: '0299998888', address: '19 Glenmore Road', suburb: 'Paddington', state: 'NSW' , postcode: '2021')
 
 user1 = User.create!(email: 'admin1@mobeeas.com', password: 'KFmiwork$16')
 user1.add_role :admin
+admin1 = Admin.create!(user_id: user1.id, first_name: 'Ann', last_name: 'Alama', phone: '0299998888', address: '2 Holden Street', suburb: 'Ashfield', state: 'NSW' , postcode: '2031')
 
 user2 = User.create!(email: 'admin2@mobeeas.com', password: 'KFmiwork$16')
 user2.add_role :admin
+admin2 = Admin.create!(user_id: user2.id, first_name: 'Annette', last_name: 'Ackerman', phone: '0299998888', address: '19 Fifth Avenue', suburb: 'Denistone', state: 'NSW' , postcode: '2114')
 
 user3 = User.create!(email: 'admin3@mobeeas.com', password: 'KFmiwork$16')
 user3.add_role :admin
+admin3 = Admin.create!(user_id: user3.id, first_name: 'Anita', last_name: 'Anderson', phone: '0299998888', address: '72 Hotham Parade', suburb: 'Artarmon', state: 'NSW' , postcode: '2064')
 
-user2 = User.create!(email: 'candidate@example.com', password: '12345678')
-user2.add_role :candidate
+user4 = User.create!(email: 'bronwyn@mobeeas.com', password: 'KFmiwork$16')
+user4.add_role :admin
+admin4 = Admin.create!(user_id: user4.id, first_name: 'Bronwyn', last_name: 'Moreton', phone: '0299998888', address: 'PO Box 282', suburb: 'Gordon', state: 'NSW' , postcode: '2072')
+
+user5 = User.create!(email: 'candidate1@mobeeas.com', password: '12345678')
+user5.add_role :candidate
+candidate1 = Profile.create!(user_id: user5.id, first_name: 'Candice', last_name: 'Colder', bio: 'This is the story about me.', address: '32 Pyree Street', suburb: 'Bangor', state: 'NSW', postcode: '2234', country: 'AU')
+
+user6 = User.create!(email: 'candidate2@mobeeas.com', password: '12345678')
+user6.add_role :candidate
+candidate2 = Profile.create!(user_id: user6.id, first_name: 'Carl', last_name: 'Christianson', bio: 'This is the story about me.', address: '3 Underwood Road', suburb: 'St Clair', state: 'NSW', postcode: '2759', country: 'AU')
+
+user7 = User.create!(email: 'candidate3@mobeeas.com', password: '12345678')
+user7.add_role :candidate
+candidate3 = Profile.create!(user_id: user7.id, first_name: 'Cindy', last_name: 'Clark', bio: 'This is the story about me.', address: '178 Anzac Pde', suburb: 'Kensington', state: 'NSW', postcode: '2033', country: 'AU')
+
+user8 = User.create!(email: 'candidate4@mobeeas.com', password: '12345678')
+user8.add_role :candidate
+candidate4 = Profile.create!(user_id: user8.id, first_name: 'Charlie', last_name: 'Camberwell', bio: 'This is the story about me.', address: '235 Annandale St,', suburb: 'Annandale', state: 'NSW', postcode: '2038', country: 'AU')
+
+user9 = User.create!(email: 'host-independent1@mobeeas.com', password: '12345678')
+user9.add_role :host
+host_independent1 = OrgUserProfile.create!(user_id: user9.id, first_name: 'Helen', last_name: 'Hunt', phone: '0299998888', position: 'parent', suburb: 'Redfern', state: 'NSW', postcode: '2016', country: 'AU', agency: 'I am an Independent')
+
+user10 = User.create!(email: 'host-independent2@mobeeas.com', password: '12345678')
+user10.add_role :host
+host_independent2 = OrgUserProfile.create!(user_id: user10.id, first_name: 'Hugh', last_name: 'Hewson', phone: '0299998888', position: 'parent', suburb: 'Mascot', state: 'NSW', postcode: '2020', country: 'AU', agency: 'I am an Independent')
+
+user11 = User.create!(email: 'host-org1@mobeeas.com', password: '12345678')
+user11.add_role :host
+host_org1 = OrgUserProfile.create!(user_id: user11.id, first_name: 'Harry', last_name: 'Hart', phone: '0299998888', position: 'SAM', suburb: 'Wahroonga', state: 'NSW', postcode: '2076', country: 'AU', agency: 'I represent an Organisation')
+
+user12 = User.create!(email: 'host-org2@mobeeas.com', password: '12345678')
+user12.add_role :host
+host_org2 = OrgUserProfile.create!(user_id: user12.id, first_name: 'Helen', last_name: 'Hamden', phone: '0299998888', position: 'SAM', suburb: 'Lindfield', state: 'NSW', postcode: '2070', country: 'AU', agency: 'I represent an Organisation')
+
+user13 = User.create!(email: 'sponsor1@mobeeas.com', password: '12345678')
+user13.add_role :sponsor
+sponsor1 = Sponsor.create!(user_id: user13.id, first_name: 'Sam', last_name: 'Sampson', organisation: 'SparkSales', phone1: '0299999999', address: '111 Church Street', suburb: 'Parramatta', state: 'NSW', postcode: '2150', country: 'AU', position: 'Manager')
+
+user14 = User.create!(email: 'sponsor2@mobeeas.com', password: '12345678')
+user14.add_role :sponsor
+sponsor2 = Sponsor.create!(user_id: user14.id, first_name: 'Sonia', last_name: 'Stratton', organisation: 'StudioSeven', phone1: '0299999999', address: '100 Carillon Ave', suburb: 'Newtown', state: 'NSW', postcode: '2042', country: 'AU', position: 'Executive')
+
 # Profile.create(user_id: user2.id, )
 OrganisationType.create!(name: 'School', description: 'Primary, Middle and High Schools')
 OrganisationType.create!(name: 'University', description: 'Australian University')
@@ -218,11 +270,6 @@ SchoolYear.create!(name: 'Beyond Year 12')
 EngagementTokenPack.create!(name: 'One token', price_ex_gst: 70.00, number_of_tokens: 1)
 EngagementTokenPack.create!(name: 'Two tokens', price_ex_gst: 120.00, number_of_tokens: 2)
 EngagementTokenPack.create!(name: 'Five tokens', price_ex_gst: 280.00, number_of_tokens: 5)
-
-AchievementLevel.create!(name: 'Causing Concern')
-AchievementLevel.create!(name: 'Working towards')
-AchievementLevel.create!(name: 'Achieved')
-AchievementLevel.create!(name: 'Going Beyond')
 
 SubscriptionPack.create!(name: 'organisation', price_ex_gst: 200.00)
 SubscriptionPack.create!(name: 'independent', price_ex_gst: 80.00)
