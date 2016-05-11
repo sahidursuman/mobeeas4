ff7140c182d5d528f1778e855e5b91d6afd36ce1
ksilitonga@hotmail.com <ksilitonga@hotmail.com>
2016-05-11 14:17:11 +1000

    removed edit login link from homepage and added edit login link from candidate sidebar, host sidebar, sponsor sidebar and admin sidebar. After user edited login, he will be redirected to his profile page.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,4 +7,21 @@ class RegistrationsController < Devise::RegistrationsController
     my_roles_path
   end
 
+  # This will redirect user to his current profile after the user updated the login email or password
+  def after_update_path_for(current_user)
+    if (current_user.has_role? :host) && (current_user.org_user_profile.present?)
+      @org_user_profile = OrgUserProfile.find(current_user.org_user_profile.id)
+      host_profile_path
+    elsif (current_user.has_role? :candidate) && (current_user.profile.present?)
+      @profile = Profile.find(current_user.profile.id)
+      candidate_profile_path
+    elsif (current_user.has_role? :sponsor) && (current_user.sponsor.present?)
+      @sponsor = Sponsor.find(current_user.sponsor.id)
+      sponsor_path(@sponsor)
+    elsif (current_user.has_role? :admin) && (current_user.admin.present?)
+      @admin = Admin.find(current_user.admin.id)
+      admin_page_path
+    end
+  end
+
 end
